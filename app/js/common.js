cfb920b2aeab52d753526adaa2ef5a0776a09ab6
Semen Milokhin <semen.milokhin@yandex.ru>
2020-06-15 13:57:00 +0300

    day 2 save

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	initCommonSliders();
+	initSelects();
 	
 	function initCommonSliders() {
 		var sliders = $('.common-slider');
@@ -26,9 +27,61 @@ $(document).ready(function(){
 					nextArrow: nextArr
 				});
 			}
-			
-			console.log(controls);
+		});
+	}
+	function initSelects() {
+		var selects = $('.select');
 
+		selects.each(function(selectIndex,select) {
+			var selectItem       = $(select),
+				selectLabel      = selectItem.find('.select__label'),
+				selectLabelValue = selectLabel.find('.select__label-value'),
+				selectInput      = selectItem.find('.select__hidden-input'),
+				selectList       = selectItem.find('.select__list'),
+				selectListItem   = selectList.find('.select__list-item'),
+				closeSelect = function() {
+					selectList.css({
+						clip: 'rect(0, 9999px , 0, 0)'
+					});
+					selectItem.removeClass('opened');
+				},
+				openSelect = function() {
+					selectList.css({
+						clip: 'rect(0, 9999px , '+selectList.outerHeight(true)+'px , 0)'
+					});
+					selectItem.addClass('opened');
+				},
+				closeAllSelects = function(count) {
+					selects.each(function(c,elem) {
+						if(count !== c) {
+							$(elem).find('.select__list').css({
+								clip: 'rect(0, 9999px , 0, 0)'
+							});
+							$(elem).removeClass('opened');
+						}
+					});
+				};
+
+			selectLabel.on('click', function(evt) {
+				closeAllSelects(selectIndex);
+				evt.preventDefault();
+				if (!selectItem.hasClass('opened')) {
+					openSelect();
+				} else {
+					closeSelect();
+				};
+			});
+
+			selectListItem.each(function(i,el) {
+				$(el).on('click', function(evt) {
+					var value = $(el).attr('data-value'),
+						text  = $(el).text();
+					evt.preventDefault();
+					selectInput.val(value);
+					selectLabelValue.text(text);
+					closeSelect();
+				})
+			})
 		});
 	}
 });
