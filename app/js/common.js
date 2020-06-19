f0beba75f034f0fd79ff55fc1c30f031caf178cd
Semen Milokhin <semen.milokhin@yandex.ru>
2020-06-20 00:50:01 +0300

    save with edits

--- app/js/common.js
+++ app/js/common.js
@@ -18,24 +18,25 @@ $(document).ready(function(){
 			var slides = $(slider).find('.common-slider__slides'),
 				controls = $(slider).find('.common-slider__controls'),
 				prevArr = controls.find('.common-slider__arrow_prev'),
-				nextArr = controls.find('.common-slider__arrow_next');
+				nextArr = controls.find('.common-slider__arrow_next'),
+				dotsValue = false,
+				autoplayValue = false;
 
 			if ($(slider).hasClass('common-slider_dots')) {
-				slides.slick({
-					arrows: true,
-					dots: true,
-					draggable: false,
-					prevArrow: prevArr,
-					nextArrow: nextArr
-				});
-			} else {
-				slides.slick({
-					arrows: true,
-					draggable: false,
-					prevArrow: prevArr,
-					nextArrow: nextArr
-				});
+				dotsValue = true;
 			}
+			if ($(slider).hasClass('common-slider_autoplay')) {
+				autoplayValue = true;
+			}
+
+			slides.slick({
+				arrows: true,
+				dots: dotsValue,
+				autoplay: autoplayValue,
+				draggable: false,
+				prevArrow: prevArr,
+				nextArrow: nextArr
+			});
 		});
 	}
 	function initSelects() {
